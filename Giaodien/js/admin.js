a7883186b56fb1ed8d66d6a4f07e7ab409967684
u34811 <u34811@UMC-C998>
2021-06-28 11:30:15 +0700

    Thêm giao diện admin

--- Giaodien/js/admin.js
+++ Giaodien/js/admin.js
@@ -3,8 +3,11 @@ $(function () {
     var listStep = [{
         index: 0,
         key: 'step-1',
-        name: 'Dept Manager'
-    
+        name: 'Dept Manager',
+        return: {
+
+        }
+
     },
     {
         index: 1,
@@ -13,9 +16,8 @@ $(function () {
         return: {
             "name": "Back to",
             "items": {
-                "step-1": { "name": "Dept Manager" },
-                "step-2": { "name": "Assets" },
-                "step-3": { "name": "Factory Manager" }
+                "step-1": { "name": "Dept Manager" }
+
             }
         }
     },
@@ -40,7 +42,8 @@ $(function () {
             "name": "Back to",
             "items": {
                 "step-1": { "name": "Dept Manager" },
-                "step-2": { "name": "Assets" }
+                "step-2": { "name": "Assets" },
+                "step-3": { "name": "Factory Manager" }
             }
         }
     },
@@ -53,7 +56,10 @@ $(function () {
             "name": "Back to",
             "items": {
                 "step-1": { "name": "Dept Manager" },
-                "step-2": { "name": "Assets" }
+                "step-2": { "name": "Assets" },
+                "step-3": { "name": "Factory Manager" },
+                "step-4": { "name": "General Director" }
+
             }
         }
     },
@@ -66,8 +72,13 @@ $(function () {
             "name": "Back to",
             "items": {
                 "step-1": { "name": "Dept Manager" },
-                "step-2": { "name": "Assets" }
+                "step-2": { "name": "Assets" },
+                "step-3": { "name": "Factory Manager" },
+                "step-4": { "name": "General Director" },
+                "step-5": { "name": "Purchasing Dept" }
+
             }
+
         }
     },
 
@@ -79,7 +90,11 @@ $(function () {
             "name": "Back to",
             "items": {
                 "step-1": { "name": "Dept Manager" },
-                "step-2": { "name": "Assets" }
+                "step-2": { "name": "Assets" },
+                "step-3": { "name": "Factory Manager" },
+                "step-4": { "name": "General Director" },
+                "step-5": { "name": "Purchasing Dept" },
+                "step-6": { "name": "Applicant" }
             }
         }
     }
@@ -95,17 +110,6 @@ $(function () {
 
     });
     $('#submitors').hide();
-    $('.steps-step a').on("click", function () {
-        $(".steps-step a").each(function (index) {
-            $(this).removeClass('btn-success')
-        });
-        $(this).addClass('btn-success')
-        current_step = $(this).text();
-        resetList();
-
-        $("#submitors").fadeOut(1);
-        $("#submitors").fadeIn();
-    })
 
     function resetList() {
         $('#listUser .form-check-input').prop('checked', false);
@@ -154,26 +158,97 @@ $(function () {
             }
         }
     });
-    $.each(listStep, function (index,value) {
-        if(value.return != null){
+    var startX = 50;
+    var startY = 100;
+    var pi = 15;
+
+    $.each(listStep, function (index, value) {
+
+        var step = makeSVG('circle', { cx: startX, cy: startY, r: 15, fill: '#43a047', stroke: 'white', 'stroke-width': 2, class: "steps-step context-menu-" + (index + 1) });
+        var step_text = makeSVG('text', { x: startX - 4, y: startY + 5, fill: 'white', class: "context-menu-" + (index + 1) })
+        step_text.appendChild(document.createTextNode(index + 1));
+        if (index < listStep.length - 1) {
+            var line = makeSVG('line', { x1: startX + pi, y1: startY, x2: 100 + startX + pi, y2: startY, style: "stroke:rgb(141, 132, 132);stroke-width:0.5" })
+            $('svg').append(line);
+        }
+        var step_name = makeSVG('text', { x: startX - pi * 2, y: startY + 30, fill: 'gray' })
+        step_name.appendChild(document.createTextNode(value.name));
+        $('svg').append(step);
+        $('svg').append(step_text);
+        $('svg').append(step_name);
+
+        startX = startX + pi + 100;
+
+        if (value.return != null) {
             $.contextMenu({
                 selector: '.context-menu-' + (index + 1),
                 callback: function (key, options) {
-                    var step = listStep.find(x => x.key === key);
-                    alert(step.name);
+                    var stepStart = listStep.find(x => x.index === index);
+                    if (key == "exit") {
+                        for (var i = 0; i < 7; i++) {
+                            $('.context-menu-' + (i + 1)).css({ stroke: "white" });
+                        }
+                        $('#submitors').hide();
+
+                    } else if (key == "selectUser") {
+                        current_step = stepStart.index + 1;
+                        resetList();
+                        for (var i = 0; i < 7; i++) {
+                            $('.context-menu-' + (i + 1)).css({ stroke: "white" });
+                        }
+                        $('.context-menu-' + (index + 1)).css({ stroke: "#003d00" });
+                        $("#submitors").fadeOut(1);
+                        $("#submitors").fadeIn();
+                    } else {
+                        var step = listStep.find(x => x.key === key);
+
+                        if (stepStart.returnTo != null) {
+                            removeReturnLine(stepStart.index, stepStart.returnTo);
+                        }
+                        stepStart.returnTo = step.index;
+                        drawReturnLine(index, step.index);
+                    }
+
                 },
                 items: {
-    
-                    "Return":value.return 
+
+                    "Return": value.return,
+                    "selectUser": { "name": "Select approval" },
+                    "exit": { "name": "Exit" }
                 }
             });
         }
-       
+
     })
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.moveTo(0,0);
-    ctx.lineTo(200,100);
-    ctx.stroke();
 
 });
+function drawReturnLine(currentStation, returnStation) {
+    var pi = 15;
+    var startX = 50 + 115 * currentStation;
+    var returnX = 50 + 115 * returnStation;
+    var startY = 100;
+    var initY = 1 + returnStation * 10;
+    var color = '#111';
+
+    var line1 = makeSVG('line', { x1: startX, y1: startY - pi, x2: startX, y2: initY, style: "stroke:" + color + ";stroke-width:0.5", class: "line-" + currentStation + "-" + returnStation })
+    var line2 = makeSVG('line', { x1: startX, y1: initY, x2: returnX, y2: initY, style: "stroke:" + color + ";stroke-width:0.5", class: "line-" + currentStation + "-" + returnStation })
+    var line3 = makeSVG('line', { x1: returnX, y1: initY, x2: returnX, y2: startY - pi, style: "stroke:" + color + ";stroke-width:0.5", class: "line-" + currentStation + "-" + returnStation })
+    var line4 = makeSVG('line', { x1: returnX, y1: startY - pi, x2: returnX - 7, y2: startY - pi - 4, style: "stroke:" + color + ";stroke-width:0.5", class: "line-" + currentStation + "-" + returnStation })
+    var line5 = makeSVG('line', { x1: returnX, y1: startY - pi, x2: returnX + 7, y2: startY - pi - 4, style: "stroke:" + color + ";stroke-width:0.5", class: "line-" + currentStation + "-" + returnStation })
+
+    $('svg').append(line1);
+    $('svg').append(line2);
+    $('svg').append(line3);
+    $('svg').append(line4);
+    $('svg').append(line5);
+
+}
+function removeReturnLine(currentStation, returnStation) {
+    $(".line-" + currentStation + "-" + returnStation).remove();
+}
+function makeSVG(tag, attrs) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for (var k in attrs)
+        el.setAttribute(k, attrs[k]);
+    return el;
+}
